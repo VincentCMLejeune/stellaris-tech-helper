7e345325605fb5d179440ffbc0cf0fcef30280b9
Vincent Lejeune <vincentcmlejeune@gmail.com>
2023-11-18 19:07:10 +0100

    Refactored App file

--- stellaris-tech-helper/src/index.js
+++ stellaris-tech-helper/src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// COMPONENTS
+import App from "./App";
+import Techs from "./pages/techs/Techs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/hello",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/techs",
+    element: <Techs />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
